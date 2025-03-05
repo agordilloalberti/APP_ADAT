a04e2e1ee15a7fbd37cb18bb085114dc94c94798
Alvarogogal <agorsua0610@g.educaand.es>
2025-03-05 20:33:09 +0100

    .

--- src/main/kotlin/com/es/aplicacion/error/exception/BadRequestException.kt
+++ src/main/kotlin/com/es/aplicacion/error/exception/BadRequestException.kt
@@ -1,4 +1,5 @@
 package com.es.aplicacion.error.exception
 
-class BadRequestException(message: String) : Exception("Not authorized exception (401). $message") {
+class BadRequestException(message: String)
+    : Exception("Not authorized exception (401). $message") {
 }
